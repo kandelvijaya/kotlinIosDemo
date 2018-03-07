2e0086c328c85050543a4be6369598225ad02e71
vkandel <vijaya.prakash.kandel@zalando.de>
2018-03-07 11:05:03 +0100

    added sample xcode project

---
+++ KotlinIOS/KotlinIOS/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2018 com.kandelvijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Some placeholder"
+    }
+
+}
+
